596ea7952685d139e83226c1e43a13b035af5167
Nacho Verdón <nverblaz@gmail.com>
2018-10-21 20:28:07 +0200

    intro scene, falcon win audio

---
+++ UnityProject/MV_JAM_4/Assets/Scripts/Intro.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Intro : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Invoke("LoadGameScene", 3f);
+	}
+	
+	// Update is called once per frame
+	void LoadGameScene () {
+        SceneManager.LoadScene("GameScene");
+	}
+}
